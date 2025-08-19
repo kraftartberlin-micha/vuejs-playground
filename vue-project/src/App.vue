c25b7f16bf84262693c161ec12b8d182ed1e4663
Michael Kraft <michael.kraft@discounto.de>
2025-08-20 01:02:10 +0200

    removed container setup, different tutorials out of date or not compatible
    first vue beginner demo with junus ergin, https://www.youtube.com/watch?v=UDWfize-zIE

---
+++ vue-project/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <RouterView/>
+</template>
+
+<script>
+export default{
+  components:{
+    Header
+  }
+}
+</script>
+
+<style>
+body, #app {
+  font-family: "Open Sans", sans-serif;
+  margin: 0;
+  padding: 0;
+    input {
+      border: 0;
+      border-radius: 8px;
+      width: 100%;
+  }
+}
+</style>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <RecipeItem :image="recipe.image" :label="recipe.name" :attributes="recipe.difficulty" v-for="recipe in recipes"/>
+</template>
+
+<script>
+import RecipeItem from "@/components/RecipeItem.vue";
+
+export default {
+  components: {
+    RecipeItem
+  },
+  data() {
+    return {recipes: []}
+  },
+  methods: {
+    async getRecipes() {
+      const query = 'mar';
+      const url = 'https://dummyjson.com/recipes/search?q=' + query
+      let resp = await fetch(url)
+      resp.json().then((data) => {
+        this.recipes = data.recipes
+        console.log(this.recipes)
+      });
+    }
+  },
+  mounted() {
+    this.getRecipes()
+  }
+}
+</script>
